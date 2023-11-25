c6e9a357b7c67fe19632d7d92d91179a89dea8c1
jdun24 <34287430+jdun24@users.noreply.github.com>
2023-11-24 19:11:45 -0700

    Update server.py

--- server.py
+++ server.py
@@ -3,45 +3,45 @@ import struct
 import sys
 import packet
 import player
+from player import Player
 from util import handleCommand
 
+import pdb
 ECHOMAX = 1024  # Adjust this to your maximum packet size
 
 def main():
-        # Check for the correct number of parameters
-        if len(sys.argv) != 2:
-                sys.stderr.write("Usage: {} <UDP SERVER PORT>\n".format(sys.argv[0]))
-                sys.exit(1)
+	# Check for the correct number of parameters
+	if len(sys.argv) != 2:
+		sys.stderr.write("Usage: {} <UDP SERVER PORT>\n".format(sys.argv[0]))
+		sys.exit(1)
 
-        echoServPort = int(sys.argv[1])  # First argument: local port
+	echoServPort = int(sys.argv[1])  # First argument: local port
 
-        # Create a socket for sending/receiving datagrams
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	# Create a socket for sending/receiving datagrams
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-        # Bind to the local address
-        sock.bind(('', echoServPort))
+	# Bind to the local address
+	sock.bind(('', echoServPort))
 
-        print("Server is listening on port:", echoServPort)
+	print("Server is listening on port:", echoServPort)
 
+	registeredPlayers = []
+	registeredGames = []
 
-        registeredPlayers = []
+	while True:  # Run forever
+		# Block until receive a message from a client
+		data, clientAddr = sock.recvfrom(ECHOMAX)
 
-        while True:  # Run forever
-                # Block until receive a message from a client
-                data, clientAddr = sock.recvfrom(ECHOMAX)
+		# Unpack the received data into the packet structure
+		gamePacket = packet.SamplePacket.from_bytes(data)
 
-                # Unpack the received data into the packet structure
-                gamePacket = packet.SamplePacket.from_bytes(data)
+		#pdb.set_trace()
+		if handleCommand(registeredPlayers, registeredGames, gamePacket , sock, clientAddr) == 1:
+			print("Successful CommandHandle on IP address:", clientAddr[0], "\n")
+		else:
+			print("server.py, main():- handleCommand() FAILED\n")
+			sock.sendto(packet.SamplePacket(0, "", -2, -2, 0, 1).to_bytes() , clientAddr)
 
-
-                if handleCommand( registeredPlayers, gamePacket , sock, clientAddr) == 1:
-                        print("Successful CommandHandle on IP address:", clientAddr[0])
-                else:
-                        print("server.py, main():- handleCommand() FAILED\n")
-
-
-                # Send the received datagram back to the client
-                sock.sendto(data, clientAddr)
 
 if __name__ == "__main__":
-        main()
+	main()
